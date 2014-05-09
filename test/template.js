8d0461634693d986caee6aa9724438f3512246a1
Alexander G. Ramirez <agramirez2001@gmail.com>
2014-05-09 04:44:22 +0000

    updated template file for agr-logger-helpers functions

--- test/template.js
+++ test/template.js
@@ -1,9 +1,7 @@
-/**
-  Tests for the the agr-logger constructor primary functionality.  
-*/
+/** Tests for agr-logger-helpers.FunctionName */
 'use strict';
 
-var Logger = require('../lib/agr-logger.js');
+var helpers = require('../lib/agr-logger-helpers.js').internal;
 
 exports.constructor = {
   setUp: function(done) {
@@ -14,7 +12,73 @@ exports.constructor = {
 
     data = null;
     actual = 'object';
-    expected = typeof new Logger();
+    expected = helpers.FunctionName();
+
+    test.expect(1);
+    test.equal(actual, expected, 'should work without any problems');
+    test.done();
+  },
+  'null args': function(test) {
+    var data, actual, expected;
+
+    data = null;
+    actual = 'object';
+    expected = helpers.FunctionName(data);
+
+    test.expect(1);
+    test.equal(actual, expected, 'should work without any problems');
+    test.done();
+  },
+  'single object args': function(test) {
+    var data, actual, expected;
+
+    data = {some: 'data'};
+    actual = 'object';
+    expected = helpers.FunctionName(data);
+
+    test.expect(1);
+    test.equal(actual, expected, 'should work without any problems');
+    test.done();
+  },
+  'single array args': function(test) {
+    var data, actual, expected;
+
+    data = [1,23,45,1];
+    actual = 'object';
+    expected = helpers.FunctionName(data);
+
+    test.expect(1);
+    test.equal(actual, expected, 'should work without any problems');
+    test.done();
+  },
+  'single string args': function(test) {
+    var data, actual, expected;
+
+    data = 'some text';
+    actual = 'object';
+    expected = helpers.FunctionName(data);
+
+    test.expect(1);
+    test.equal(actual, expected, 'should work without any problems');
+    test.done();
+  },
+  'single number args': function(test) {
+    var data, actual, expected;
+
+    data = 12345;
+    actual = 'object';
+    expected = helpers.FunctionName(data);
+
+    test.expect(1);
+    test.equal(actual, expected, 'should work without any problems');
+    test.done();
+  },
+  'single boolan args': function(test) {
+    var data, actual, expected;
+
+    data = true;
+    actual = 'object';
+    expected = helpers.FunctionName(data);
 
     test.expect(1);
     test.equal(actual, expected, 'should work without any problems');
